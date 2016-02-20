c39da377d3ace363bd0c59846c6d8901c525deb3
wantonsolutions <stewbertgrant@gmail.com>
2016-02-20 15:36:00 -0800

    modified db with zones and accounts

--- initdb.sql
+++ initdb.sql
@@ -7,11 +7,17 @@ use Gibil;
 drop table if exists Event;
 create table Event
 	/*(cid int auto_increment not null,*/
-	(category int not null,
-	zone int not null,
-	panel int not null,
+	(account int not null,
+	alarmzone char(1) not null,
+	alarmstate char(1) not null,
+	supervisoryzone char(1) not null,
+	supervisorystate char(1) not null,
+	troublezone char(1) not null,
+	troublestate char(1) not null,
+	powerzone char(1) not null,
+	powerstate char(1) not null,
 	timestamp varchar(64) not null,
-    status varchar(64),
-	primary key (zone, panel));
+    	message varchar(64),
+	primary key (account));
 
 commit;
