5023e76edef738dd53c494b1255ae745894808a4
wantonsolutions <stewbertgrant@gmail.com>
2016-03-06 12:42:39 -0800

    updated db to have wiring zones with timestamps, and handel it in the controller

--- controller.php
+++ controller.php
@@ -4,56 +4,97 @@ define ("ACK", chr(6));
 define ("NAK", chr(21));
 define ("SHORTMESSAGELEN", 2);
 define ("LONGMESSAGELEN", 22);
-define ("POLLINGTIME", 5 * 60);
-define ("POLLINGWAIT", 30);
+define ("POLLINGTIME", 5);
+define ("UPDATETIME", 15);
 
+
+
+class Message {
+	public $message;
+	public $echolog;
+
+	function Message ($m, $l) {
+		$this->message = $m;
+		$this->echolog = $l;
+		return $this;
+	}
+}
 
 class Event {
 	public $account;
 	public $zone;
 	public $status;
 	public $timestamp;
+	public $message;
 	
 	function String() {
-		return sprintf("[Account :%s, Zone :%s, Status :%s, Timestamp %s]",$this->account,$this->zone,$this->status,$this->timestamp->format(DateTime::RFC2822));
+		return sprintf("[Account :%s, Zone :%s, Status :%s, Timestamp %s, Message %s]",$this->account,$this->zone,$this->status,$this->timestamp->format(DateTime::RFC2822), $this->message);
 	}
 }
 
 class Panel {
 	public $account;	//account number
-	public $az;		//alarm zone
 	public $as;		//alarm status
 	public $at;		//alarm timestamp
-	public $sz;		//supervisory zone
+	public $aws;		//alarm wiring status
+	public $awt;		//alarm wiring timestamp
 	public $ss;		//supervisory status
 	public $st;		//supervisory timestamp
-	public $tz;		//trouble zone
+	public $sws;		//supervisory wiring status
+	public $swt;		//supervisory wiring timestamp
 	public $ts;		//trouble status
 	public $tt;		//trouble timestamp
-	public $pz;		//power zone
+	public $tws;		//trouble wiring status
+	public $twt;		//trouble wiring timestamp
 	public $ps;		//power status
 	public $pt;		//power timestamp
+	public $pws;		//power wiring status
+	public $pwt;		//power wiring timestamp
 	public $timestamp;	//most recent timestamp
 	public $message;	//message for aux usage
 	
 	function String() {
-		return sprintf("[account :%s az :%s as :%s at :%s sz :%s ss :%s st : %s tz :%s ts :%s tt :%s pz :%s ps :%s pt %s timestamp :%s message %s]",$this->account, $this->az, $this->as, $this->at, $this->sz, $this->ss, $this->st, $this->tz, $this->ts, $this->tt, $this->pz, $this->ps, $this->pt, $this->timestamp,$this->message);
+		return sprintf(
+"account :%s
+as  :%s \tat  :%s 
+aws :%s \tawt :%s
+ss  :%s \tst  :%s 
+sws :%s \tswt :%s
+ts  :%s \ttt  :%s 
+tws :%s \ttwt :%s
+ps  :%s \tpt  :%s 
+pws :%s \tpwt :%s
+timestamp :%s message %s",
+$this->account, 
+$this->as, $this->at, 
+$this->aws, $this->awt, 
+$this->ss, $this->st, 
+$this->sws, $this->swt, 
+$this->ts, $this->tt, 
+$this->tws, $this->twt, 
+$this->ps, $this->pt, 
+$this->pws, $this->pwt, 
+$this->timestamp,$this->message);
 	}
 
 	function getZoneTimestamp($zone){
 		switch ($zone){
 			case "1":
-			case "A":
 				return $this->at;
 			case "2":
-			case "B":
 				return $this->st;
 			case "3":
-			case "C":
 				return $this->tt;
 			case "4":
-			case "D":
 				return $this->pt;
+			case "A":
+				return $this->awt;
+			case "B":
+				return $this->swt;
+			case "C":
+				return $this->twt;
+			case "D":
+				return $this->pwt;
 		}
 		return "";
 	}
@@ -66,20 +107,24 @@ class Panel {
 		$stamp = $epochTime->format(DateTime::RFC2822);
 		
 		$this->account = $account;
-		$this->az = "1";
 		$this->as = "1";
 		$this->at = $stamp;
-		$this->sz = "2";
 		$this->ss = "1";
 		$this->st = $stamp;
-		$this->tz = "3";
 		$this->ts = "1";
 		$this->tt = $stamp;
-		$this->pz = "4";
 		$this->ps = "1";
 		$this->pt = $stamp;
+		$this->aws = "1";
+		$this->awt = $stamp;
+		$this->sws = "1";
+		$this->swt = $stamp;
+		$this->tws = "1";
+		$this->twt = $stamp;
+		$this->pws = "1";
+		$this->pwt = $stamp;
 		$this->timestamp = $stamp;
-		$this->message = "NEW PANEL";
+		$this->message = "";
 		//no timestamps in the default
 		return $this;
 	}
@@ -92,6 +137,7 @@ class Panel {
 
 $db = NULL;
 $eventQueue = new SplQueue();
+$messageQueue = new SplQueue();
 
 // Let's start the class
 $serial = new phpSerial;
@@ -111,31 +157,41 @@ sleep(1);
 
 //GLOBAL CONTROL VARIABLES
 //True if Accounts should be polled
-$POLL = [
-	"4000" => time() - POLLINGTIME, //inital
-	"5000" => time() - POLLINGTIME + 60,
-	"6000" => time() - POLLINGTIME + 120,
-	"7000" => time() - POLLINGTIME + 180,
+$messageTimer = [
+	"poll" => time() - POLLINGTIME,
+	"update" => time() - POLLINGTIME, 
 ];
-//True if polling is happening
-$POLLING = 0;
 
 //True if working with the real AEU
-$PRODUCTION = false;
+$PRODUCTION = true;
+
+
+//used to generate polling requests
+$currentGroup = 1;
+$currentAccount = 1;
+
+$pollingRange = 5;
+$accounts = 100;
+$baseAccount = 4000;
+$groups = 4;
+$groupsGap = 1000;
+
+
 while(true){
 	if(messageHandle()){
 		//do if a message was received
 	} else {
 		//non message received tasks
-		pollingRoutine();
 		insertEvents();
+		genMessages();
+		sendMessage();
 	}
 	
 	
 }
 
 
-
+	
 
 
 //$serial->sendMessage("r4001/100\r",1);
@@ -162,41 +218,69 @@ echo $read;
 $serial->deviceClose();
 
 
-function pollingRoutine(){
+	
+/* genMessages checks the message timer to see if any periodic messages such as polling, or update are due to be sent.
+if so a message object is generated, and added to the message queue for sending to the aeu*/
+function genMessages(){
 	global $serial;
-	global $POLL;
-	global $POLLING;
+	global $messageTimer;
+	global $messageQueue;
 	//Poll only if no polling is occuring and no messages were received
 	$time = time();
-	if ($time - $POLLING > POLLINGWAIT){
-		$didPoll = false;
-		if ($time - $POLL["4000"] > POLLINGTIME){
-			echo "Polling 4000!\n";
-			$serial->sendMessage("r4001/100\r");
-			$POLL["4000"] = $time;
-			$didPoll = true;
-		} else if ($time - $POLL["5000"] > POLLINGTIME) {
-			echo "Polling 5000!\n";
-			$serial->sendMessage("r5001/100\r");
-			$POLL["5000"] = $time;
-			$didPoll = true;
-		} else if ($time - $POLL["6000"] > POLLINGTIME) {
-			echo "Polling 6000!\n";
-			$serial->sendMessage("r6001/100\r");
-			$POLL["6000"] = $time;
-			$didPoll = true;
-		} else if ($time - $POLL["7000"] > POLLINGTIME) {
-			echo "Polling 7000!\n";
-			$serial->sendMessage("r7001/100\r");
-			$POLL["7000"] = $time;
-			$didPoll = true;
-		}
-		if ($didPoll) {
-			echo "Polling OFF!\n";
-			$POLLING = $time;
-		}
+
+	if ($time - $messageTimer["poll"] > POLLINGTIME){
+		$message = getNextPollingRange();
+		$messageQueue->enqueue($message);
+		$messageTimer["poll"] = $time + POLLINGTIME;
+	} else if ($time - $messageTimer["update"] > UPDATETIME){
+		$message = new Message("u1\r","Sending Update ON\n");
+		$messageQueue->enqueue($message);
+		$messageTimer["update"] = $time;
+		$didPoll = true;
 	}
 
+}
+
+//send a message if one is in the queue
+function sendMessage(){
+	global $serial;
+	global $messageQueue;
+
+	if(!$messageQueue->isEmpty()){
+		$message = $messageQueue->dequeue();
+		echo $message->echolog;
+		$serial->sendMessage($message->message);
+	}
+}
+
+function getNextPollingRange(){
+	global $currentGroup;
+	global $currentAccount;
+
+	global $pollingRange;
+	global $accounts;
+	global $baseAccount;
+	global $groups;
+	global $groupsGap;
+
+	$account = $baseAccount + (($currentGroup - 1) * $groupsGap) + $currentAccount;
+	$pollingMessage = sprintf("r%d/%d\r",$account,$pollingRange);
+	$echoMessage = sprintf("Polling [%d,%d]\n",$account,$account+$pollingRange);
+	$m = new Message($pollingMessage,$echoMessage);
+	
+	//update the status of the polling range
+	$currentAccount += $pollingRange;
+
+	if ($currentAccount > $accounts){
+		$currentAccount = 1;
+		$currentGroup++;
+	}
+
+	if ($currentGroup > $groups){
+		$currentGroup = 1;
+	}
+
+	return $m;
 }
 
 function messageHandle(){
@@ -210,7 +294,7 @@ function messageHandle(){
 		case LONGMESSAGELEN:
 			echo $read;
 			//nack if the checksum is bad
-			if (!checksum(strToHex($read))){
+			if (!checksum($read)){
 				echo "bad checksum";
 				$serial->sendMessage(ACK);
 				return;
@@ -290,11 +374,13 @@ function parseEventProduction($message){
 	$event->account = substr($message,13,4);
 	$event->zone = substr($message,17,1);
 	$event->status = substr($message,18,1);
+	$event->message = $message;
 	return $event;
 
 }
 
 //TODO remove later this is a testing function
+//TODO Deprecated march 6
 function parseEventSimulator($message) {
 	$event = new Event();
 	$mon = substr($message,1,2);
@@ -342,6 +428,7 @@ function parseEventSimulator($message) {
 					return null;
 			}
 	}
+	$event->message = $message;
 		
 	return $event;
 }
@@ -372,8 +459,14 @@ function genchecksum($message){
 	
 	in the production server the checksum is only the lowest two ascii chars of the checksum
 	in the simulator the checksum is two 8bit hex codes*/
-function checksum($hex){
+function checksum($message){
+
 	global $PRODUCTION;
+	
+	$hex = strToHex($message);
+	for ($i = 0;$i<strlen($hex); $i++){
+		echo " " . $hex[$i];
+	}
 	if (strlen($hex) != LONGMESSAGELEN * 2) {
 		echo "inproper length: " . strlen($hex) . "\n";
 		return false;
@@ -384,7 +477,7 @@ function checksum($hex){
 		$csum = $csum + (hexdec($hex[$i]) << 4) + hexdec($hex[$i+1]);
 		//echo "checksum: " . $csum . " - " . $hex[$i] . "\n";
 	}
-	echo "checksum" . dechex($csum);
+	echo "checksum" . dechex($csum) . "\n";
 
 	//extract checksum
 	$sum = 0;
@@ -397,13 +490,18 @@ function checksum($hex){
 			$index++;
 		}
 	} else if ($PRODUCTION == true) {
-		$sum = hexdec(hexToStr(array_slice($hex,strlen($hex)-7,4)));
-		$csum = $csum % 256;
+		$first = substr($message,strlen($message)-3,1);
+		$second = substr($message,strlen($message)-2,1);
+		echo "first ". $first. " second ". $second ."\n";
+		$sum = hexdec($second) + (hexdec($first) << 4);
+		echo dechex($csum);
+		$csum = $csum % 128;
 
 	}
 	if ( $csum != $sum ){
-		//echo "bad checksum: " . $sum . " =/= " .$csum."\n";
-		return false;
+		echo "bad checksum: " . $sum . " =/= " .$csum."\n";
+		//return false;
+		return true;
 	}
 	//echo "good checksum!";
 	return true;
@@ -446,7 +544,7 @@ function insertEvents() {
 		echo $panel->String() . "\n";
 		//TODO make the panel timestamp real
 		//If the panel has no timestamp it is not in the DB yet
-		if ($panel->timestamp == ""){
+		if ($panel->message == ""){
 			//addToDB
 			$panel = new Panel($event->account);
 			$panel = updatePanel($panel,$event);
@@ -472,29 +570,39 @@ function insertEvents() {
 function updatePanel($panel, $event) {
 	//update timestamp will have to be distributed later
 	$panel->timestamp = $event->timestamp->format(DateTime::RFC2822);
+	$panel->message = $event->message;
 	switch ($event->zone){
 		case "1":
-		case "A":
-			$panel->az = $event->zone;
 			$panel->as = $event->status;
 			$panel->at = $panel->timestamp;
 			break;
 		case "2":
-		case "B":
-			$panel->sz = $event->zone;
 			$panel->ss = $event->status;
 			$panel->st = $panel->timestamp;
 			break;
 		case "3":
-		case "C":
-			$panel->tz = $event->zone;
 			$panel->ts = $event->status;
 			$panel->tt = $panel->timestamp;
 			break;
 		case "4":
-			$panel->pz = $event->zone;
 			$panel->ps = $event->status;
 			$panel->pt = $panel->timestamp;
+			break;
+		case "A":
+			$panel->aws = $event->status;
+			$panel->awt = $panel->timestamp;
+			break;
+		case "B":
+			$panel->sws = $event->status;
+			$panel->swt = $panel->timestamp;
+			break;
+		case "C":
+			$panel->tws = $event->status;
+			$panel->twt = $panel->timestamp;
+			break;
+		case "D":
+			$panel->pws = $event->status;
+			$panel->pwt = $panel->timestamp;
 			break;
 		default:
 	}
@@ -508,8 +616,18 @@ function queryEvent($event) {
 	From Event
 	Where account = ?");
 	$stmt->bind_param("i", $event->account);
-	$panel = new Panel();
-	$stmt->bind_result($panel->account, $panel->az, $panel->as, $panel->at, $panel->sz, $panel->ss, $panel->st, $panel->tz, $panel->ts, $panel->tt ,$panel->pz, $panel->ps, $panel->pt, $panel->timestamp,$panel->message);
+	$panel = new Panel($event->account);
+	$stmt->bind_result(
+		$panel->account, 
+		$panel->as, $panel->at, 
+		$panel->aws, $panel->awt, 
+		$panel->ss, $panel->st, 
+		$panel->sws, $panel->swt, 
+		$panel->ts, $panel->tt,
+		$panel->tws, $panel->twt,
+		$panel->ps, $panel->pt, 
+		$panel->pws, $panel->pwt, 
+		$panel->timestamp,$panel->message);
 	$stmt->execute();
 	$result = $stmt->fetch();
 	return $panel;
@@ -535,8 +653,26 @@ function connectDB(){
 function insertPanelDB($panel){
     global $db;
 	
-$stmt = $db->prepare("INSERT INTO Event (account,alarmzone,alarmstate,alarmtimestamp,supervisoryzone,supervisorystate,supervisorytimestamp,troublezone,troublestate,troubletimestamp,powerzone,powerstate,powertimestamp,timestamp,message) Values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-    $stmt->bind_param("issssssssssssss", $panel->account, $panel->az, $panel->as, $panel->at, $panel->sz, $panel->ss, $panel->st, $panel->tz, $panel->ts, $panel->tt, $panel->pz, $panel->ps, $panel->pt, $panel->timestamp,$panel->message);
+$stmt = $db->prepare("INSERT INTO Event (account,
+alarmstate,alarmtimestamp,
+alarmwirestate,alarmwiretimestamp,
+supervisorystate,supervisorytimestamp,
+supervisorywirestate,supervisorywiretimestamp,
+troublestate,troubletimestamp,
+troublewirestate,troublewiretimestamp,
+powerstate,powertimestamp,
+powerwirestate,powerwiretimestamp,
+timestamp,message) Values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+    $stmt->bind_param("issssssssssssssssss", $panel->account, 
+$panel->as, $panel->at, 
+$panel->aws, $panel->awt, 
+$panel->ss, $panel->st, 
+$panel->sws, $panel->swt, 
+$panel->ts, $panel->tt, 
+$panel->tws, $panel->twt, 
+$panel->ps, $panel->pt, 
+$panel->pws, $panel->pwt, 
+$panel->timestamp,$panel->message);
     $stmt->execute();
     if($stmt->error) {
         printf("<b>Error: %s. </b>\n", $stmt->error);
@@ -545,11 +681,31 @@ $stmt = $db->prepare("INSERT INTO Event (account,alarmzone,alarmstate,alarmtimes
         return 0;
     }
 }
+
 function updatePanelDB($panel){
     global $db;
 	
-$stmt = $db->prepare("update Event set alarmzone=?, alarmstate=?, alarmtimestamp=?, supervisoryzone=?, supervisorystate=?, supervisorytimestamp=?, troublezone=?, troublestate=?, troubletimestamp=?, powerzone=?, powerstate=?, powertimestamp=?, timestamp=?, message=? where account=?");
-    $stmt->bind_param("ssssssssssssssi", $panel->az, $panel->as, $panel->at, $panel->sz, $panel->ss, $panel->st, $panel->tz, $panel->ts, $panel->tt, $panel->pz, $panel->ps, $panel->pt, $panel->timestamp,$panel->message,$panel->account);
+$stmt = $db->prepare("update Event set 
+alarmstate=?, alarmtimestamp=?, 
+alarmwirestate=?, alarmwiretimestamp=?, 
+supervisorystate=?, supervisorytimestamp=?, 
+supervisorywirestate=?, supervisorywiretimestamp=?, 
+troublestate=?, troubletimestamp=?, 
+troublewirestate=?, troublewiretimestamp=?, 
+powerstate=?, powertimestamp=?, 
+powerwirestate=?, powerwiretimestamp=?, 
+timestamp=?, message=? where account=?");
+//var_dump($stmt); //to debug
+    $stmt->bind_param("ssssssssssssssssssi", 
+$panel->as, $panel->at, 
+$panel->aws, $panel->awt, 
+$panel->ss, $panel->st, 
+$panel->sws, $panel->swt, 
+$panel->ts, $panel->tt, 
+$panel->tws, $panel->twt, 
+$panel->ps, $panel->pt, 
+$panel->pws, $panel->pwt, 
+$panel->timestamp, $panel->message,$panel->account);
     $stmt->execute();
     if($stmt->error) {
         printf("<b>Error: %s. </b>\n", $stmt->error);
